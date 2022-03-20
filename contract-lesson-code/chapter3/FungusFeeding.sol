e6821dd9c15f3f10103fb2839c5e09227bd7c676
ch-4ml <fetch@ajou.ac.kr>
2022-03-21 02:18:33 +0900

    feat: Add source code for each chapter.

--- contract-lesson-code/chapter3/FungusFeeding.sol
+++ contract-lesson-code/chapter3/FungusFeeding.sol
@@ -2,12 +2,9 @@
 pragma solidity ^0.8.12;
 
 import "./FungusFactory.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-// 외부 컨트랙트와 상호작용하기 위한 인터페이스 정의
 interface FeedFactoryInterface {
-
-    // 외부 컨트랙트에서 필요한 함수 헤더만 선언
-    // 솔리디티 함수는 여러 개의 리턴 값을 가질 수 있음
     function getFeed(uint _id) external view returns (
         string memory name,
         uint dna,
@@ -15,28 +12,20 @@ interface FeedFactoryInterface {
     );
 }
 
-// 상속, 자식 컨트랙트는 접근 제어자가 private인 변수나 함수를 제외하고
-// 부모에 선언된 모든 함수와 변수에 접근할 수 있음
 contract FungusFeeding is FungusFactory {
     
-    // 외부 컨트랙트 인터페이스 선언
     FeedFactoryInterface feedContract;
 
-    // 함수 제어자
     modifier onlyOwnerOf(uint fungusId) {
-        // 주어진 조건을 만족하지 않으면 revert
-        require(_msgSender() == fungusToOwner[fungusId]);
-        // 다음 구문을 계속해서 진행
+        require(msg.sender == fungusToOwner[fungusId]);
         _;
     }
     
     function setFeedFactoryContractAddress(address address_) external onlyOwner {
-        // 외부 컨트랙트 객체 생성
         feedContract = FeedFactoryInterface(address_);
     }
 
-    // fungus 매개변수가 함수 내에서 변경되기 때문에 memory로 선언
-    function _triggerCooldown(Fungus memory fungus) internal view {    
+    function _triggerCooldown(Fungus memory fungus) internal view {
         fungus.readyTime = uint32(block.timestamp + cooldownTime);
     }
 
@@ -58,12 +47,9 @@ contract FungusFeeding is FungusFactory {
         _triggerCooldown(myFungus);
     }
 
-    // 함수 호출 시 송금이 필요한 함수는 payable 키워드를 이용해서 정의
     function feed(uint fungusId, uint feedId) public payable {
         uint feedDna;
         uint feedPrice;
-        
-        // 여러 개의 리턴 값 중 사용하지 않는 리턴 값은 공백으로 처리할 수 있음
         (,feedDna,feedPrice) = feedContract.getFeed(feedId);
         require(msg.value == feedPrice, "be paid inappropriate expenses");
         feedAndMultiply(fungusId, feedDna, "feed");
--- contract-lesson-code/chapter4/FungusOwnership.sol
+++ contract-lesson-code/chapter4/FungusOwnership.sol
@@ -5,12 +5,9 @@ import "./FungusFeeding.sol";
 
 contract FungusOwnership is FungusFeeding {
 
-    // 각 토큰에 대해 대리 전송 승인받은 주소를 mapping
     mapping (uint => address) private operatorApproval;
 
-    // 토큰 전송 시 발생하는 event
     event Transfer(address indexed from, address indexed to, uint256 tokenId);
-    // 토큰 전송 승인 시 발생하는 event
     event Approval(address indexed owner, address indexed approved, uint256 tokenId);
 
     function balanceOf(address owner) public view returns (uint balance) {
@@ -22,16 +19,14 @@ contract FungusOwnership is FungusFeeding {
     }
 
     function transferFrom(address from, address to, uint tokenId) public {
-        // 토큰의 소유자 또는 운영자만 호출할 수 있음
         require(_msgSender() == operatorApproval[tokenId] || _msgSender() == ownerOf(tokenId), "transferFrom caller is not owner nor approved operator");
         _transfer(from, to, tokenId);
     }
 
-    // 토큰 소유자 대신 transfer를 호출할 수 있는 (토큰 전송 승인받은) 운영자 지정
+    // 토큰 소유자 대신 transfer를 호출할 수 있는 운영자 지정
     function approve(address to, uint256 tokenId) public onlyOwnerOf(tokenId) {
         address owner = ownerOf(tokenId);
         require(to != owner, "approval to current owner");
-        // 토큰의 소유자 또는 운영자만 호출할 수 있음
         require(
             _msgSender() == owner || _msgSender() == operatorApproval[tokenId],
             "approve caller is not owner nor approved operator"
@@ -40,22 +35,15 @@ contract FungusOwnership is FungusFeeding {
         _approve(to, tokenId);
     }
 
-    // 토큰 전송
     function _transfer(address from, address to, uint tokenId) private {
-        // 반드시 보내는 주소는 해당 토큰의 소유자여야 함
         require(ownerOf(tokenId) == from, "transfer from incorrect owner");
-        // 받는 주소는 zero address가 아니어야 함, 즉 수신자가 있어야 함
         require(to != address(0), "transfer to the zero address");
         
-        // 전송한 토큰의 소유자가 변경되었기 때문에 approve를 초기화
         _approve(address(0), tokenId);
         
-        // 토큰의 보유량과 소유권 변경
         ownerFungusCount[from]--;
         ownerFungusCount[to]++;
         fungusToOwner[tokenId] = to;
-
-        // Transfer 이벤트 발생
         emit Transfer(from, to, tokenId);
     }
 
